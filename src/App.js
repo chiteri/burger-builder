726ebe1171f7a2f0f6eea73300641899c8b4c0ea
Martin Chiteri <martin.chiteri@gmail.com>
2020-10-15 13:47:49 -0600

    Start the implementation of a BurgerBuilder component with minimal styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Layout from '../src/components/Layout/Layout';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout> 
-          <p>Test content</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
