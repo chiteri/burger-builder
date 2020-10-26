83a71c22c08cdf2bab7e9f2b3aee8b704a35885a
Martin Chiteri <martin.chiteri@gmail.com>
2020-10-25 18:08:54 -0600

    Make adjustments to the logo for more responsiveness

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,9 @@ import classes from './Logo.css';
 import burgerLogo from '../../assets/images/burger-logo.png';
 
 const logo = (props) => (
-    <div className={classes.Logo}> <img src={burgerLogo} alt="MyBurger" /> </div>
+    <div className={classes.Logo} style={{height: props.height}}> 
+        <img src={burgerLogo} alt="MyBurger" /> 
+    </div>
 );
 
 export default logo;
