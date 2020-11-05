93179261137df6c8e0b8280db498a406ec0d1e76
Martin Chiteri <martin.chiteri@gmail.com>
2020-11-05 15:14:37 -0700

    Send a POST request to server with order details

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -85,7 +86,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue');
+        // alert('You continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice, 
+            customer: {
+                name: 'Martin Chiteri', 
+                address: {
+                    street: '000 - 1111, 222 Ave NW',
+                    zipCode: 'A1C B2D', 
+                    country: 'CANADA'
+                },
+                email: 'Test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        // Send data to the backend
+        axios.post('/orders.json', order)
+            .then(response => { console.log(response); })
+            .catch(error => { console.log(error); });
     }
 
     render() {
